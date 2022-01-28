6646b7b2c89f8f76b3e29bea2bf2ed8a3c9cb585
danielcr0995 <daniel.correa.1995@gmail.com>
2022-01-27 22:47:53 -0500

    create chart component

---
+++ src/dummyData.js
@@ -0,0 +1,52 @@
+export const userData = [
+    {
+      name: 'Jan',
+      "active user": 2000,
+      
+    },
+    {
+        name: 'Feb',
+        "active user": 4000,
+        
+      },{
+        name: 'Mar',
+        "active user": 5000,
+        
+      },{
+        name: 'Apr',
+        "active user": 1500,
+        
+      },{
+        name: 'May',
+        "active user": 3300,
+        
+      },{
+        name: 'Jun',
+        "active user": 4000,
+        
+      },{
+        name: 'Jul',
+        "active user": 2200,
+        
+      },{
+        name: 'Aug',
+        "active user": 4500,
+        
+      },{
+        name: 'Sep',
+        "active user": 4200,
+        
+      },{
+        name: 'Oct',
+        "active user": 2100,
+        
+      },{
+        name: 'Nov',
+        "active user": 6000,
+        
+      },{
+        name: 'Dec',
+        "active user": 1000,
+        
+      },
+  ];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Chart from '../components/chart/Chart';
 import FeatureInfo from '../components/featureInfo/FeatureInfo';
 import "./home.css"
+import {userData} from "../dummyData"
 
 export default function Home() {
   return (
     <div className="home">
         <FeatureInfo/>
+        <Chart tile="User Analytics" data={userData} dataKey="active user" grid/>
     </div>
   );
 }
